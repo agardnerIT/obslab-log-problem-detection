7ec53b7880c17b2c676172553d0b303dfe89d939
Adam Gardner <adam.gardner@dynatrace.com>
2025-04-22 17:01:38 +1000

    update test

--- .devcontainer/testing/test_dynatrace_ui_validate_observability_data.py
+++ .devcontainer/testing/test_dynatrace_ui_validate_observability_data.py
@@ -17,8 +17,7 @@ def test_dynatrace_ui(page: Page):
     # ################################################
     logger.info("Opening search menu")
     open_search_menu(page)
-    
-    
+
     # ################################################
     logger.info(f"Searching for {app_visual_name}")
     search_for(page, app_visual_name)
@@ -30,40 +29,94 @@ def test_dynatrace_ui(page: Page):
     # ################################################
     logger.info(f"{app_name} app is now displayed")
 
-    # Open tags menu
+    # Open logs menu
     wait_for_app_to_load(page, is_classic_app=False)
-    app_frame_locator, app_frame = get_app_frame_and_locator(page, is_classic_app=True)
+    app_frame_locator, app_frame = get_app_frame_and_locator(page, is_classic_app=False)
 
-    # Find and click on "Tags"
-    expect(app_frame_locator.get_by_title("Tags", exact=True).first).to_contain_text("Tags", timeout=WAIT_TIMEOUT)
-    tags_element = app_frame_locator.get_by_title("Tags", exact=True).first
-    tags_element.click()
+    # Find and click on "Logs DQL Builder" search box
+    logs_search_box = app_frame_locator.get_by_test_id("dqlbuiler-form-field")
+    logger.info(logs_search_box)
+    #expect(logs_search_box).to_be_attached(timeout=WAIT_TIMEOUT)
+    expect(logs_search_box).to_be_editable(timeout=WAIT_TIMEOUT)
+    logs_search_box.click(timeout=WAIT_TIMEOUT)
+    time.sleep(1)
+    logs_search_box.type(text="service.name=cart", delay=50)
+
+    search_button = app_frame_locator.get_by_test_id("logs-input-container__run-query")
+    search_button.click(timeout=WAIT_TIMEOUT)
+    
+    # Wait for search results
+    log_table = app_frame_locator.get_by_test_id("logs-table-search")
+    expect(log_table).to_be_visible(timeout=WAIT_TIMEOUT)
+
+    time.sleep(5)
+
+    # Open services screen
+    app_visual_name = "Services"
+    app_name = "services"
+
+    logger.info("Opening search menu")
+    open_search_menu(page)
+
+    # ################################################
+    logger.info(f"Searching for {app_visual_name}")
+    search_for(page, app_visual_name)
+
+    logger.info(f"Opening {app_visual_name} app")
+    open_app_from_search_modal(page, app_name, is_classic_app=False)
+
+    # ################################################
+    logger.info(f"{app_name} app is now displayed")
+
+    time.sleep(5)
+
+    # Open services list
+    expect(app_frame_locator.get_by_test_id("resource-health-card-surface").first).to_be_attached(timeout=WAIT_TIMEOUT)
+    services_list_box = app_frame_locator.get_by_test_id("resource-health-card-surface").first
+    expect(services_list_box).to_be_visible(timeout=WAIT_TIMEOUT)
+    services_list_box.click()
+    #time.sleep(5)
+
+    # Wait for services app to load
+    services_header = app_frame_locator.get_by_test_id("dql-table-header")
+    expect(services_header).to_have_text("Services", timeout=WAIT_TIMEOUT)
+    #time.sleep(5)
+
+    cart_service_hyperlink = app_frame_locator.get_by_test_id("entity-details-button").get_by_text("cart")
+    expect(cart_service_hyperlink).to_be_attached(timeout=WAIT_TIMEOUT)
+    cart_service_hyperlink.click()
+    time.sleep(5)
+
+
+    #expect(app_frame_locator.get_by_title("Tags", exact=True).first).to_contain_text("Tags", timeout=WAIT_TIMEOUT)
+    #tags_element = app_frame_locator.get_by_title("Tags", exact=True).first
+    #tags_element.click()
 
     # Find and click on "Automatically applied tags"
-    automatically_applied_tags_element = app_frame_locator.get_by_title("Automatically applied tags", exact=True).first
-    expect(automatically_applied_tags_element).to_contain_text("Automatically applied tags", timeout=WAIT_TIMEOUT)
-    automatically_applied_tags_element.click()
+    # automatically_applied_tags_element = app_frame_locator.get_by_title("Automatically applied tags", exact=True).first
+    # expect(automatically_applied_tags_element).to_contain_text("Automatically applied tags", timeout=WAIT_TIMEOUT)
+    # automatically_applied_tags_element.click()
 
-    # Open invidual tag dropdown
-    tag_name = "crossplane-created"
+    # # Open invidual tag dropdown
+    # tag_name = "crossplane-created"
 
-    # Expect tag NOT to exist
-    # loc = app_frame_locator.locator(f"dtx-markdown[uitestid=\"cell-summary\"] div.content:has-text(\"{tag_name}\")")
-    # loc = app_frame.locator(f"dtx-markdown[uitestid=\"cell-summary\"]")
-    # Locate the outer iframe
-    #outer_frame = app_frame_locator
+    # # Expect tag NOT to exist
+    # # loc = app_frame_locator.locator(f"dtx-markdown[uitestid=\"cell-summary\"] div.content:has-text(\"{tag_name}\")")
+    # # loc = app_frame.locator(f"dtx-markdown[uitestid=\"cell-summary\"]")
+    # # Locate the outer iframe
+    # #outer_frame = app_frame_locator
 
-    # Quite honestly, I have no idea why this first line
-    # is necessary, but 4 days into this and this is the only reliable way I can find to make this work.
-    # I can't waste any more time on this, so this will do
-    FOUND_ITEM = True
-    try:
-        page.locator("[data-testid=\"cluster-wrapper-app-iframe\"]").content_frame.get_by_text(tag_name, exact=True).wait_for(timeout=WAIT_TIMEOUT)
-    except Exception as te:
-        FOUND_ITEM = False
-        logger.info(f"Tried waiting for {tag_name} and couldn't find it (this is a good thing)")
+    # # Quite honestly, I have no idea why this first line
+    # # is necessary, but 4 days into this and this is the only reliable way I can find to make this work.
+    # # I can't waste any more time on this, so this will do
+    # FOUND_ITEM = True
+    # try:
+    #     page.locator("[data-testid=\"cluster-wrapper-app-iframe\"]").content_frame.get_by_text(tag_name, exact=True).wait_for(timeout=WAIT_TIMEOUT)
+    # except Exception as te:
+    #     FOUND_ITEM = False
+    #     logger.info(f"Tried waiting for {tag_name} and couldn't find it (this is a good thing)")
 
-    expect(page.locator("[data-testid=\"cluster-wrapper-app-iframe\"]").content_frame.get_by_text(tag_name, exact=True)).not_to_be_visible(timeout=WAIT_TIMEOUT)
+    # expect(page.locator("[data-testid=\"cluster-wrapper-app-iframe\"]").content_frame.get_by_text(tag_name, exact=True)).not_to_be_visible(timeout=WAIT_TIMEOUT)
 
     # try:
         
