f8b0afb5912ca05417e03754f4559503de06b546
Adam Gardner <adam.gardner@dynatrace.com>
2025-04-28 14:32:21 +1000

    add UI script to validate observability

--- .devcontainer/testing/test_dynatrace_ui_validate_observability_data.py
+++ .devcontainer/testing/test_dynatrace_ui_validate_observability_data.py
@@ -68,118 +68,40 @@ def test_dynatrace_ui(page: Page):
     # ################################################
     logger.info(f"{app_name} app is now displayed")
 
-    time.sleep(5)
-
     # Open services list
     expect(app_frame_locator.get_by_test_id("resource-health-card-surface").first).to_be_attached(timeout=WAIT_TIMEOUT)
     services_list_box = app_frame_locator.get_by_test_id("resource-health-card-surface").first
     expect(services_list_box).to_be_visible(timeout=WAIT_TIMEOUT)
     services_list_box.click()
-    #time.sleep(5)
 
     # Wait for services app to load
     services_header = app_frame_locator.get_by_test_id("dql-table-header")
     expect(services_header).to_have_text("Services", timeout=WAIT_TIMEOUT)
-    #time.sleep(5)
 
     cart_service_hyperlink = app_frame_locator.get_by_test_id("entity-details-button").get_by_text("cart")
     expect(cart_service_hyperlink).to_be_attached(timeout=WAIT_TIMEOUT)
     cart_service_hyperlink.click()
+
+    # Open the notebooks app
+    app_visual_name = "Notebooks"
+    app_name = "notebooks"
+
+    logger.info("Opening search menu")
+    open_search_menu(page)
+
+    # ################################################
+    logger.info(f"Searching for {app_visual_name}")
+    search_for(page, app_visual_name)
+
+    logger.info(f"Opening {app_visual_name} app")
+    open_app_from_search_modal(page, app_name, is_classic_app=False)
+
+    # ################################################
+    logger.info(f"{app_name} app is now displayed")
+
     time.sleep(5)
 
-
-    #expect(app_frame_locator.get_by_title("Tags", exact=True).first).to_contain_text("Tags", timeout=WAIT_TIMEOUT)
-    #tags_element = app_frame_locator.get_by_title("Tags", exact=True).first
-    #tags_element.click()
-
-    # Find and click on "Automatically applied tags"
-    # automatically_applied_tags_element = app_frame_locator.get_by_title("Automatically applied tags", exact=True).first
-    # expect(automatically_applied_tags_element).to_contain_text("Automatically applied tags", timeout=WAIT_TIMEOUT)
-    # automatically_applied_tags_element.click()
-
-    # # Open invidual tag dropdown
-    # tag_name = "crossplane-created"
-
-    # # Expect tag NOT to exist
-    # # loc = app_frame_locator.locator(f"dtx-markdown[uitestid=\"cell-summary\"] div.content:has-text(\"{tag_name}\")")
-    # # loc = app_frame.locator(f"dtx-markdown[uitestid=\"cell-summary\"]")
-    # # Locate the outer iframe
-    # #outer_frame = app_frame_locator
-
-    # # Quite honestly, I have no idea why this first line
-    # # is necessary, but 4 days into this and this is the only reliable way I can find to make this work.
-    # # I can't waste any more time on this, so this will do
-    # FOUND_ITEM = True
-    # try:
-    #     page.locator("[data-testid=\"cluster-wrapper-app-iframe\"]").content_frame.get_by_text(tag_name, exact=True).wait_for(timeout=WAIT_TIMEOUT)
-    # except Exception as te:
-    #     FOUND_ITEM = False
-    #     logger.info(f"Tried waiting for {tag_name} and couldn't find it (this is a good thing)")
-
-    # expect(page.locator("[data-testid=\"cluster-wrapper-app-iframe\"]").content_frame.get_by_text(tag_name, exact=True)).not_to_be_visible(timeout=WAIT_TIMEOUT)
-
-    # try:
-        
-    # except AssertionError as ae:
-    #     logger.info(ae)
-    #     pytest
-    
-    # try:
-    #     #item = page.locator("[data-testid=\"cluster-wrapper-app-iframe\"]").content_frame.get_by_text(tag_name, exact=True)
-    #     #logger.info(f"Item Count: {item.count()}. Text Content: {item.text_content()}")
-    #     #time.sleep(5)
-    #     # Quite honestly, I have no idea why this first line
-    #     # is necessary, but 4 days into this and this is the only reliable way I can find to make this work.
-    #     # I can't waste any more time on this, so this will do
-    #     page.locator("[data-testid=\"cluster-wrapper-app-iframe\"]").content_frame.get_by_text(tag_name, exact=True).wait_for(timeout=WAIT_TIMEOUT)
-    #     expect(page.locator("[data-testid=\"cluster-wrapper-app-iframe\"]").content_frame.get_by_text(tag_name, exact=True)).not_to_be_visible(timeout=WAIT_TIMEOUT)
-    #     #page.locator("[data-testid=\"cluster-wrapper-app-iframe\"]").content_frame.get_by_role("row", name="crossplane-created2").get_by_role("button").click()
-    #     #page.locator("[data-testid=\"cluster-wrapper-app-iframe\"]").content_frame.get_by_role("row", name="crossplane-created2 Show").locator("dt-expandable-cell").get_by_role("button").click()
-    #     # page.locator("[data-testid=\"cluster-wrapper-app-iframe\"]").content_frame.get_by_role("row", name="crossplane-created", exact=True).get_by_role("button").click()
-    #     # page.locator("[data-testid=\"cluster-wrapper-app-iframe\"]").content_frame.locator("form-readonly").filter(has_text="crossplane-created2").dblclick()
-    #     # time.sleep(2)
-    #     #loc = app_frame_locator.locator('[uitestid="cell-summary"]').get_by_text(tag_name)
-    #     #loc = app_frame_locator.get_by_role("row", name=tag_name, exact=True)
-    #     #logger.info(loc.all())
-    #     #logger.info(loc.all_inner_texts())
-    #     #logger.info(loc.all_text_contents())
-    # except Exception as e:
-    #     logger.info(e)
-    #     logger.info("Could not find element (this is a good thing).")
-    # finally:
-    #     logger.info("Test completed. Tag removed.")
-        
-    
-    # page.frame_locator('//div//dtx-markdown')
-    # loc = app_frame_locator.locator('//div//dtx-markdown')
-    
-    # #loc = app_frame_locator.get_by_text("crossplane-created")
-    # #frame_loc = page.frame_locator("div")
-    # #frame_loc.
-    # #loc = outer_frame.locator('dtx-markdown[uitestid="cell-summary"]')
-    # #loc = outer_frame.locator('div')
-    # #frame_loc.get
-    # #frame_loc.a
-
-    # #logger.info(outer_frame)
-    # logger.info("HERE #1")
-    # logger.info(loc.all())
-    # logger.info("HERE #2")
-
-    # # Locate the inner iframe within the outer iframe
-    # #inner_frame = outer_frame.frame_locator('#app-iframe')
-
-    # # Wait for the <div> element containing the text "crossplane-created" to be present
-    # #inner_frame.locator('dtx-markdown[uitestid="cell-summary"] div.content:has-text("crossplane-created")').wait_for(state='visible')
-    # #loc = inner_frame.locator('dtx-markdown[uitestid="cell-summary"]')
-    # #loc.all()
-
-    
-    # #loc = app_frame_locator.frame_locator("dtx-markdown[uitestid=\"cell-summary\"]").
-    # logger.info(loc.all())
-    # if len(loc.all()) == 0:
-    #     logger.info(f"Could not find {tag_name} (this is a good thing)")
-    # else:
-    #     logger.error(f"{tag_name} was found (not good). Error")
-    #     pytest.fail(reason=f"{tag_name} WAS found (and shouldn't have been)")
-    #expect(app_frame_locator.locator(f"dtx-markdown[uitestid=\"cell-summary\"] div.content:has-text(\"{tag_name}\")")).not_to_have_text(tag_name, timeout=WAIT_TIMEOUT)
+    create_new_document(page=page, close_microguide=True)
+    add_document_section(page=page, section_type_text=SECTION_TYPE_DQL)
+    enter_dql_query(page=page, dql_query=retrieve_dql_query("fetch cart service logs"), section_index=0, validate=True)
+    delete_document(page=page)
